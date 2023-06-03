f082e76ff8928a76ad772c95fe0a3cb033bd9542
hein-htut-aung <zugoripls@gmail.com>
2023-06-03 14:01:09 +0000

    Deploying to gh-pages from @ hein-htut-aung/create-typescript-app@fcba27921d194185bc5cd9a98c166c4f3648d6b5 🚀

---
+++ index-81f6ea38.js
@@ -0,0 +1,2 @@
+var e="styles_demo-img__zKPrg";function a(a){console.debug("Module loaded!");const d=document.createElement("img");d.onload=()=>{null==a||a.replaceChildren(d)},d.classList.add(e),d.src="/assets/c31f3febfdaea576.png"}export{a as default};
+//# sourceMappingURL=index-81f6ea38.js.map
